d97f90a4cf427fa320d900083c64b3148df9c79c
pengnanfang <1064977639@qq.com>
2021-01-17 22:10:32 +0800

    scope

---
+++ scope/scope.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def test_scope():
+    def do_local():
+        spam = "local spam"
+
+    def do_nonlocal():
+        nonlocal spam
+        spam = "nonlocal spam"
+
+    def do_global():
+        global spam
+        spam = "global spam"
+
+    spam = "test spam"
